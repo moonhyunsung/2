402006521539639ac1cc152319dabe00525aaf06
hsmoon <hsmoon@e-fnrnd.com>
2021-08-24 10:24:50 +0900

    Initial commit

---
+++ app/src/main/java/com/cj/myapplication/fnrnd/MainActivity2.java
@@ -0,0 +1,16 @@
+package com.cj.myapplication.fnrnd;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.cj.myapplication.R;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
